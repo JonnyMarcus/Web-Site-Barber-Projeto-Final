cdf97845f04f7fc863d316fc2e906fbd6d1cbb14
Ana Cristina Meira <140420502+anacmeira@users.noreply.github.com>
2026-08-13 19:37:00 -0300

    feat: adiciona backend strapi com schemas de barbeiros, servicos e planos

---
+++ backend-barber/src/api/barbeiro/controllers/barbeiro.ts
@@ -0,0 +1,7 @@
+/**
+ * barbeiro controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::barbeiro.barbeiro');
---
+++ backend-barber/src/api/barbeiro/routes/barbeiro.ts
@@ -0,0 +1,7 @@
+/**
+ * barbeiro router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::barbeiro.barbeiro');
